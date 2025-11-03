be2a62260dae8c56f75f2ddf77855cb266d02aea
silviatools <silviatools@mail.ru>
2025-11-03 22:54:07 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const GITHUB_CONFIG = {
+  OWNER: 'silviatools',  // Ваш никнейм на GitHub
+  REPO: 'gazelle-loader',   // Название репо
+  TOKEN: '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',      // НОВЫЙ токен (создайте его!)
+  BRANCH: 'main',
+  PRESETS_FOLDER: 'presets'
+};
